76f8a71cf76fe9cef2e0379cb8526bdfe259943f
Raghu K <raghuk.vit@gmail.com>
2022-12-08 06:20:40 +0530

    Update roboshop-kubernetes-infra/

--- main.tf
+++ main.tf
@@ -70,17 +70,6 @@ module "rabbitmq" {
   vpc_cidr        = element([for i, j in module.vpc : j.vpc_cidr], 0)
 }
 
-module "alb" {
-  source   = "./vendor/modules/alb"
-  for_each = local.merged_alb
-  env      = var.env
-  subnets  = each.value.subnets
-  name     = each.key
-  vpc_id   = element([for i, j in module.vpc : j.vpc_id], 0)
-  vpc_cidr = element([for i, j in module.vpc : j.vpc_cidr], 0)
-  internal = each.value.internal
-}
-
 module "EKS" {
   source                  = "./vendor/modules/eks/"
   ENV                     = var.env
